7a9deb6a7963bce0bd6d28c4ae5b6b2471db7a18
Anand Shekhar <66034323+anandshekhr@users.noreply.github.com>
2025-08-03 12:58:34 +0530

    UPDATE Footer

--- shop-app/src/app/shared/constants/api.constants.ts
+++ shop-app/src/app/shared/constants/api.constants.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:8000';
+export const API_BASE_URL = 'https://api.vamsmechatronica.in';
 
 export const API_ENDPOINTS = {
   CATEGORIES: `${API_BASE_URL}/api/v1/categories/`,
