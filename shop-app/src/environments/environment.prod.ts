a0d01cff1935195e1e583c791c9ac521c82a7233
Anand Shekhar <66034323+anandshekhr@users.noreply.github.com>
2025-10-23 00:15:11 +0530

    UPDATE main url

--- shop-app/src/environments/environment.prod.ts
+++ shop-app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://api.vamsmechatronica.in",
+  apiUrl: "https://api.thenearbyshop.com",
 };
--- shop-app/src/environments/environment.ts
+++ shop-app/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: "https://api.vamsmechatronica.in",
+  apiUrl: "https://api.thenearbyshop.com",
 };
